2c0bd4267e39d69635a3cc945fb4913e61b96a77
T-Kononok <t.k.17.11.2000@gmail.com>
2020-05-06 23:09:05 +0300

    добавлена кнопка профиля

--- src/MainFrameClient.java
+++ src/MainFrameClient.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import javax.swing.plaf.BorderUIResource;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class MainFrameClient extends JFrame {
 
@@ -11,6 +14,7 @@ public class MainFrameClient extends JFrame {
 
     private CardLayout cardsUp = new CardLayout();
     private CardLayout cardsDown = new CardLayout();
+    private JPanel menuPanel = new JPanel();
     private JPanel cardsUpPanel = new JPanel(cardsUp);
     private JPanel cardsDownPanel = new JPanel(cardsDown);
 
@@ -21,33 +25,47 @@ public class MainFrameClient extends JFrame {
         Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - getWidth()) / 2,
                     (kit.getScreenSize().height - getHeight()) / 2);
-
-//        Up
-        JPanel loginUpPanel = new JPanel();
-        loginUpPanel.setLayout(new BoxLayout(loginUpPanel, BoxLayout.X_AXIS));
-        loginUpPanel.setBackground(BLUEVK);
+        
+//        Menu
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.X_AXIS));
+        menuPanel.setBackground(BLUEVK);
         JLabel titlLabel = new JLabel("Жалкая пародия на VK");
         titlLabel.setForeground(Color.WHITE);
         titlLabel.setFont(new Font("Tahoma",Font.BOLD,15));
         JLabel hashtagLabel = new JLabel("#лучшедома");
         hashtagLabel.setForeground(Color.WHITE);
-        hashtagLabel.setFont(new Font("Tahoma",Font.BOLD,12));
-        loginUpPanel.add(new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/vkIm.png")));
-        loginUpPanel.add(hashtagLabel);
-        loginUpPanel.add(Box.createHorizontalGlue());
-        loginUpPanel.add(titlLabel);
-        loginUpPanel.add(Box.createHorizontalGlue());
+        hashtagLabel.setFont(new Font("Tahoma",Font.BOLD,13));
+        menuPanel.add(new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/vkIm.png")));
+        menuPanel.add(hashtagLabel);
+        menuPanel.add(Box.createHorizontalGlue());
+        menuPanel.add(titlLabel);
+        menuPanel.add(Box.createHorizontalGlue());
         JLabel kostylLabel = new JLabel("#лучшедома");
         kostylLabel.setForeground(BLUEVK);
         kostylLabel.setFont(new Font("Tahoma",Font.BOLD,12));
-        loginUpPanel.add(kostylLabel);
+        menuPanel.add(kostylLabel);
         JLabel kostylIm = new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/kostyl.png"));
-        loginUpPanel.add(kostylIm);
+        menuPanel.add(kostylIm);
+        JLabel profileButtonLabel = new JLabel("Анонимус");
+        profileButtonLabel.setForeground(Color.WHITE);
+        profileButtonLabel.setFont(new Font("Tahoma",Font.BOLD,13));
+        JButton profileButton = new JButton();
+        profileButton.add(profileButtonLabel);
+        JLabel im = new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/kostyl2.png"));
+        im.setVisible(false);
+        profileButton.add(im);
+        profileButton.setBorderPainted(false);
+        profileButton.setBackground(BLUEVK);
+        profileButton.addMouseListener(new ButtonMouseListener());
+        menuPanel.add(profileButton);
+        
+//        Up
+        JPanel loginUpPanel = new JPanel();
         cardsUpPanel.add(loginUpPanel, "login");
 
         JPanel searchUpPanel = new JPanel();
         cardsUpPanel.add(searchUpPanel, "search");
-
+        
         JPanel chatUpPanel = new JPanel();
         cardsUpPanel.add(chatUpPanel, "chat");
 
@@ -56,8 +74,8 @@ public class MainFrameClient extends JFrame {
         loginDownPanel.setBackground(WHITEVK);
         cardsDownPanel.add(loginDownPanel, "login");
 
-        JPanel settingDownPanel = new JPanel();
-        cardsDownPanel.add(settingDownPanel, "setting");
+        JPanel profileDownPanel = new JPanel();
+        cardsDownPanel.add(profileDownPanel, "profile");
 
         JPanel searchDownPanel = new JPanel();
         cardsDownPanel.add(searchDownPanel, "search");
@@ -67,16 +85,38 @@ public class MainFrameClient extends JFrame {
 
         JPanel chatDownPanel = new JPanel();
         cardsDownPanel.add(chatDownPanel, "chat");
-//        cardsDown.show(cardsDownPanel,"messages");
+
+        cardsUp.show(cardsUpPanel,"login");
+        cardsDown.show(cardsDownPanel,"login");
 
 //        JPanel menuPanel = new JPanel();
-//        JButton settingButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/settingIm"));
-//        //settingButton.addActionListener(e -> sendMessage());
-//        JButton settingButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/settingIm"));
-//        //settingButton.addActionListener(e -> sendMessage());
+//        JButton profileButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/profileIm"));
+//        //profileButton.addActionListener(e -> sendMessage());
+//        JButton profileButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/profileIm"));
+//        //profileButton.addActionListener(e -> sendMessage());
 
-        getContentPane().add(cardsUpPanel, BorderLayout.NORTH);
-        getContentPane().add(cardsDownPanel, BorderLayout.CENTER);
+        getContentPane().add(menuPanel, BorderLayout.NORTH);
+        getContentPane().add(cardsUpPanel, BorderLayout.CENTER);
+        getContentPane().add(cardsDownPanel, BorderLayout.SOUTH);
+    }
+
+    public static class ButtonMouseListener implements MouseListener {
+
+        public void mouseClicked(MouseEvent e) { }
+
+        public void mouseEntered(MouseEvent arg0) {
+            JButton button = (JButton) arg0.getSource();
+            button.setBackground(new Color(61,104,152));
+        }
+
+        public void mouseExited(MouseEvent arg0) {
+            JButton button = (JButton) arg0.getSource();
+            button.setBackground(BLUEVK);
+        }
+
+        public void mousePressed(MouseEvent e) { }
+
+        public void mouseReleased(MouseEvent e) { }
     }
 
     public static void main(String[] args) {
