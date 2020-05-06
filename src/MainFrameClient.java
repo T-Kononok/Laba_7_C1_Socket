c8ef593407fdb42c2de4c4a6491e60551c1865cd
T-Kononok <t.k.17.11.2000@gmail.com>
2020-05-06 20:51:48 +0300

    добавлен заголовок login-а

--- src/MainFrameClient.java
+++ src/MainFrameClient.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
+import javax.swing.plaf.BorderUIResource;
 import java.awt.*;
 
 public class MainFrameClient extends JFrame {
 
-    private static final String FRAME_TITLE = "Жалкая породия на нормальный мессенджер";
-    private static final int FRAME_MINIMUM_WIDTH = 500;
-    private static final int FRAME_MINIMUM_HEIGHT = 500;
+    private static final int FRAME_WIDTH = 565;
+    private static final int FRAME_HEIGHT = 565;
+    private static final Color BLUEVK = new Color(74,118,168);
+    private static final Color WHITEVK = new Color(237,238,240);
 
     private CardLayout cardsUp = new CardLayout();
     private CardLayout cardsDown = new CardLayout();
@@ -14,31 +16,58 @@ public class MainFrameClient extends JFrame {
 
     private MainFrameClient(){
 
-        super(FRAME_TITLE);
-        setMinimumSize(new Dimension(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT));
+        setResizable(false);
+        setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - getWidth()) / 2,
                     (kit.getScreenSize().height - getHeight()) / 2);
 
 //        Up
         JPanel loginUpPanel = new JPanel();
+        loginUpPanel.setLayout(new BoxLayout(loginUpPanel, BoxLayout.X_AXIS));
+        loginUpPanel.setBackground(BLUEVK);
+        JLabel titlLabel = new JLabel("Жалкая пародия на VK");
+        titlLabel.setForeground(Color.WHITE);
+        titlLabel.setFont(new Font("Tahoma",Font.BOLD,15));
+        JLabel hashtagLabel = new JLabel("#лучшедома");
+        hashtagLabel.setForeground(Color.WHITE);
+        hashtagLabel.setFont(new Font("Tahoma",Font.BOLD,12));
+        loginUpPanel.add(new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/vkIm.png")));
+        loginUpPanel.add(hashtagLabel);
+        loginUpPanel.add(Box.createHorizontalGlue());
+        loginUpPanel.add(titlLabel);
+        loginUpPanel.add(Box.createHorizontalGlue());
+        JLabel kostylLabel = new JLabel("#лучшедома");
+        kostylLabel.setForeground(BLUEVK);
+        kostylLabel.setFont(new Font("Tahoma",Font.BOLD,12));
+        loginUpPanel.add(kostylLabel);
+        JLabel kostylIm = new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/kostyl.png"));
+        loginUpPanel.add(kostylIm);
         cardsUpPanel.add(loginUpPanel, "login");
+
         JPanel searchUpPanel = new JPanel();
         cardsUpPanel.add(searchUpPanel, "search");
+
         JPanel chatUpPanel = new JPanel();
         cardsUpPanel.add(chatUpPanel, "chat");
 
 //        Down
-        JPanel loginDowmPanel = new JPanel();
-        cardsDownPanel.add(loginDowmPanel, "login");
-        JPanel settingDowmPanel = new JPanel();
-        cardsDownPanel.add(settingDowmPanel, "setting");
+        JPanel loginDownPanel = new JPanel();
+        loginDownPanel.setBackground(WHITEVK);
+        cardsDownPanel.add(loginDownPanel, "login");
+
+        JPanel settingDownPanel = new JPanel();
+        cardsDownPanel.add(settingDownPanel, "setting");
+
         JPanel searchDownPanel = new JPanel();
         cardsDownPanel.add(searchDownPanel, "search");
+
         JPanel messagesDownPanel = new JPanel();
         cardsDownPanel.add(messagesDownPanel, "messages");
+
         JPanel chatDownPanel = new JPanel();
         cardsDownPanel.add(chatDownPanel, "chat");
+//        cardsDown.show(cardsDownPanel,"messages");
 
 //        JPanel menuPanel = new JPanel();
 //        JButton settingButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/settingIm"));
@@ -46,8 +75,8 @@ public class MainFrameClient extends JFrame {
 //        JButton settingButton = new JButton(new ImageIcon("D:/Джава/Laba_7_C1_Socket/settingIm"));
 //        //settingButton.addActionListener(e -> sendMessage());
 
-        getContentPane().add(cardsUpPanel, BorderLayout.SOUTH);
-        getContentPane().add(cardsDownPanel, BorderLayout.NORTH);
+        getContentPane().add(cardsUpPanel, BorderLayout.NORTH);
+        getContentPane().add(cardsDownPanel, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
