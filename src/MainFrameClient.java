f710a011616eff6256b5aa64c9e39828cc603959
T-Kononok <t.k.17.11.2000@gmail.com>
2020-05-07 16:45:24 +0300

    сделано окно входа

--- src/MainFrameClient.java
+++ src/MainFrameClient.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import javax.swing.plaf.BorderUIResource;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.ParseException;
 
 public class MainFrameClient extends JFrame {
 
@@ -9,6 +11,7 @@ public class MainFrameClient extends JFrame {
     private static final int FRAME_HEIGHT = 565;
     private static final Color BLUEVK = new Color(74,118,168);
     private static final Color WHITEVK = new Color(237,238,240);
+    private static final Color GREYVK = new Color(153,145,146);
 
     private CardLayout cardsUp = new CardLayout();
     private CardLayout cardsDown = new CardLayout();
@@ -18,7 +21,7 @@ public class MainFrameClient extends JFrame {
 
     private MainFrameClient(){
 
-        setResizable(false);
+//        setResizable(false);
         setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
         Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - getWidth()) / 2,
@@ -75,26 +78,100 @@ public class MainFrameClient extends JFrame {
 
 //        Down
         JPanel loginDownPanel = new JPanel();
-        loginDownPanel.setBackground(WHITEVK);
+        loginDownPanel.setBackground(Color.WHITE);
         JTextField kostylNameLoginField = new JTextField("",1);
         kostylNameLoginField.setBorder(BorderFactory.createEmptyBorder());
-        kostylNameLoginField.setBackground(WHITEVK);
-        kostylNameLoginField.setCaretColor(WHITEVK);
-        Box loginVBox = Box.createVerticalBox();
+        kostylNameLoginField.setBackground(Color.WHITE);
+        kostylNameLoginField.setCaretColor(Color.WHITE);
         JLabel vhodLabel = new JLabel("Вход пародии ВКонтакте");
         vhodLabel.setFont(new Font("Tahoma",Font.PLAIN,20));
-        JTextField nameLoginField = new JTextField("Имя",25);
+        JTextField nameLoginField = new JTextField("Имя",10);
+        nameLoginField.setFont(new Font("Tahoma",Font.PLAIN,12));
+        nameLoginField.setForeground(GREYVK);
         nameLoginField.addFocusListener(new TextFieldFocusAdapter("Имя"));
-        JTextField surnameLoginField = new JTextField("Фамилия", 25);
+        JTextField surnameLoginField = new JTextField("Фамилия", 10);
+        surnameLoginField.setFont(new Font("Tahoma",Font.PLAIN,12));
+        surnameLoginField.setForeground(GREYVK);
         surnameLoginField.addFocusListener(new TextFieldFocusAdapter("Фамилия"));
-        JButton vhodButton = new JButton("Войти");
+        MaskFormatter ipFormatter = null;
+        try {
+            ipFormatter = new MaskFormatter("###.###.##.##");
+            ipFormatter.setPlaceholderCharacter('-');
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        JFormattedTextField ipFormatTextField = new JFormattedTextField(ipFormatter);
+        ipFormatTextField.setFont(new Font("Tahoma",Font.PLAIN,12));
+        ipFormatTextField.setColumns(13);
+        ipFormatTextField.setEnabled(false);
+        JCheckBox regCheckBox = new JCheckBox();
+        regCheckBox.setBackground(Color.WHITE);
+        Image checkBoxIm = new ImageIcon("D:/Джава/Laba_7_C1_Socket/checkBoxIm.png").getImage().
+                getScaledInstance(17, 17, Image.SCALE_DEFAULT);
+        regCheckBox.setIcon(new ImageIcon(checkBoxIm));
+        regCheckBox.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                Image checkBoxIm2 = new ImageIcon("D:/Джава/Laba_7_C1_Socket/checkBoxImSelect.png").getImage().
+                        getScaledInstance(17, 17, Image.SCALE_DEFAULT);
+                regCheckBox.setIcon(new ImageIcon(checkBoxIm2));
+                ipFormatTextField.setEnabled(true);
+            } else {
+                Image checkBoxIm2 = new ImageIcon("D:/Джава/Laba_7_C1_Socket/checkBoxIm.png").getImage().
+                        getScaledInstance(17, 17, Image.SCALE_DEFAULT);
+                regCheckBox.setIcon(new ImageIcon(checkBoxIm2));
+                ipFormatTextField.setEnabled(false);
+            }
+        });
+        JLabel regLabel = new JLabel("Регистрация");
+        regLabel.setFont(new Font("Tahoma",Font.PLAIN,12));
+        JButton vhodButton = new JButton(new ImageIcon(new ImageIcon("D:/Джава/Laba_7_C1_Socket/buttonIm.png").getImage().
+                getScaledInstance(110, 40, Image.SCALE_DEFAULT)));
+        vhodButton.setBorder(BorderFactory.createEmptyBorder());
+        vhodButton.setRolloverIcon(new ImageIcon(new ImageIcon("D:/Джава/Laba_7_C1_Socket/buttonImSelect.png").getImage().
+                getScaledInstance(110, 40, Image.SCALE_DEFAULT)));
+        Box loginVBox = Box.createVerticalBox();
         loginVBox.add(kostylNameLoginField);
-        loginVBox.add(vhodLabel);
+        Box vhodLabelBox = Box.createHorizontalBox();
+        vhodLabelBox.add(Box.createHorizontalGlue());
+        vhodLabelBox.add(vhodLabel);
+        vhodLabelBox.add(Box.createHorizontalGlue());
+        loginVBox.add(vhodLabelBox);
+        loginVBox.add(Box.createVerticalStrut(10));
         loginVBox.add(nameLoginField);
+        loginVBox.add(Box.createVerticalStrut(10));
         loginVBox.add(surnameLoginField);
-        loginVBox.add(vhodButton);
+        loginVBox.add(Box.createVerticalStrut(10));
+        loginVBox.add(ipFormatTextField);
+        loginVBox.add(Box.createVerticalStrut(10));
+        Box regBox = Box.createHorizontalBox();
+        regBox.add(vhodButton);
+//        regBox.add(Box.createHorizontalStrut(10));
+        regBox.add(Box.createHorizontalGlue());
+        regBox.add(regCheckBox);
+        regBox.add(regLabel);
+        loginVBox.add(regBox);
+        loginVBox.add(Box.createVerticalStrut(200));
         loginDownPanel.add(loginVBox);
-        cardsDownPanel.add(loginDownPanel, "login");
+        Box fonHorLogin = Box.createHorizontalBox();
+        JPanel fonLoginPanel2 = new JPanel();
+        fonLoginPanel2.setBackground(WHITEVK);
+        JPanel fonLoginPanel3 = new JPanel();
+        fonLoginPanel3.setBackground(WHITEVK);
+        JPanel fonLoginPanel4 = new JPanel();
+        fonLoginPanel4.setBackground(Color.WHITE);
+        fonLoginPanel4.add(loginVBox);
+        fonHorLogin.add(fonLoginPanel2);
+        fonHorLogin.add(fonLoginPanel4);
+        fonHorLogin.add(fonLoginPanel3);
+        Box fon = Box.createVerticalBox();
+        fon.add(fonHorLogin);
+        JPanel fonLoginPanel5 = new JPanel();
+        fonLoginPanel5.setBackground(WHITEVK);
+        JLabel im2 = new JLabel(new ImageIcon("D:/Джава/Laba_7_C1_Socket/kostyl3.png"));
+        fonLoginPanel5.add(im2);
+        fon.add(fonLoginPanel5);
+        cardsDownPanel.add(fon, "login");
 
         JPanel profileDownPanel = new JPanel();
         cardsDownPanel.add(profileDownPanel, "profile");
@@ -135,12 +212,14 @@ public class MainFrameClient extends JFrame {
             JTextField field = (JTextField) e.getSource();
             if (field.getText().equals(text)) {
                 field.setText("");
+                field.setForeground(Color.BLACK);
             }
         }
         public void focusLost(FocusEvent e) {
             JTextField field = (JTextField) e.getSource();
             if (field.getText().isEmpty()) {
                 field.setText(text);
+                field.setForeground(GREYVK);
             }
         }
     }
